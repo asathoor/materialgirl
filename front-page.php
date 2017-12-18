8a7a21259eb66ee27f6dc76133e233c08f720f60
asathoor <asathoor@gmail.com>
2017-12-18 20:46:23 +0100

    front-page.php and template part

--- front-page.php
+++ front-page.php
@@ -1,16 +1,12 @@
-<h1>Hello</h1>
 <?php
 /**
- * The template for displaying all single posts
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ * file: front-page.php
+ * purpose: the front page
  *
  * @package materialgirl
  */
 
 get_header(); ?>
-
-<div class="container">
 
 	<div class="row">
 		<!-- THE CONTENT -->
@@ -19,17 +15,15 @@ get_header(); ?>
 				<main id="main" class="site-main">
 		
 				<?php while ( have_posts() ) : the_post(); ?>
-				
 							
-				
 					<?php 
-					get_template_part( 'template-parts/content', get_post_type() );
+					get_template_part( 'template-parts/content', 'frontpage' );
 		
 					the_post_navigation();
 		
 					// If comments are open or we have at least one comment, load up the comment template.
 					if ( comments_open() || get_comments_number() ) :
-						comments_template();
+						//comments_template();
 					endif;
 		
 				endwhile; // End of the loop.
@@ -39,11 +33,11 @@ get_header(); ?>
 		</div><!-- .col (content) -->
 		
 		<!-- THE SIDEBAR -->
-		<div class="col s12 m5">
+		<div class="col s12">
 		
 			<?php get_sidebar(); ?>
 			
 		</div><!-- /sidebar -->
 	</div><!-- .row -->
-</div><!-- container -->
+
 <?php get_footer(); ?>
